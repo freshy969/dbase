f1c4ef825781e63a6f71b367c729c6b6de8c465f
Frank Eno ~ XScoder <39766031+xscoder1@users.noreply.github.com>
2022-01-07 15:49:35 +0100

    Delete files directory

--- files/_Tables/get-tables.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$files = array();
-	$jsonFiles = glob('*.json');
-	for ($i=0; $i<count($jsonFiles); $i++) {
-		$jFile = str_replace( '.json', '', $jsonFiles[$i] );
-		echo $jFile.',';
-	}
-?>
